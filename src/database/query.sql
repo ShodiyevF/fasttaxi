7eec1a6c0dfae85c3c995b9cc40f7cb5726cda32
ShodiyevF <axadmirzaboyev@gmail.com>
2022-07-07 04:26:24 +0500

    add some function and uuid

--- src/database/query.sql
+++ src/database/query.sql
@@ -1,17 +1,21 @@
 create database fasttaxi;
 
-create table step(
+drop table if exists steps;
+create table steps(
     step_id int generated always as identity primary key,
     step_status smallint not null,
     step_step smallint not null,
-    step_user_telegram_id text not null
+    step_user text not null
 );
 
+drop table if exists elontaxi;
 create table elontaxi(
     taxi_id int generated always as identity primary key,
-    taxi_now varchar(120) not null,
-    taxi_then varchar(120) not null,
-    taxi_count smallint not null,
-    taxi_time text not null,
-    taxi_owner text not null
+    taxi_unique_id text unique,
+    taxi_now varchar(120),
+    taxi_then varchar(120),
+    taxi_count smallint,
+    taxi_time text,
+    taxi_owner text,
+    taxi_status smallint default 1
 );
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const telegramBot = require('node-telegram-bot-api')
+const { v4: uuidv4 } = require('uuid');
 const { uniqRow } = require('./lib/pg')
 
 const bot = new telegramBot('5053155270:AAGkjgcCFjyI-BY9Fp0vp5-3G-mlLxyg1hw', {
@@ -8,6 +9,14 @@ const bot = new telegramBot('5053155270:AAGkjgcCFjyI-BY9Fp0vp5-3G-mlLxyg1hw', {
 bot.onText(/\/start/, async (msg) => {
     try {
         const id = msg.chat.id
+
+        const orders = await uniqRow('select * from elontaxi where taxi_owner = $1', id)
+        if (orders.rows.length) {
+            await bot.sendMessage(id, 'Buyurtmalar bekor qilindi !')
+            await uniqRow('delete from elontaxi where taxi_owner = $1', id)
+        }
+        
+        
         await bot.sendMessage(id, 'TEXT', {
             reply_markup: {
                 resize_keyboard: true,
@@ -17,33 +26,94 @@ bot.onText(/\/start/, async (msg) => {
             }
         })
     } catch (error) {
-        console.log(error.message, 'Elon Taxi 🚖');
+        console.log(error.message, 'start');
     }
 })
 
-bot.onText(/\Elon Taxi 🚖/, async (msg) => {
+// bot.onText(/\Elon Taxi 🚖/, async (msg) => {
+//     try {
+//         const id = msg.chat.id
+//         await bot.sendMessage(id, 'boradigan manzilingizni kiriting', {
+//             reply_markup: {
+//                 remove_keyboard: true
+//             }
+//         })
+//         const step = (await uniqRow('select * from steps where step_user = $1 and step_status = 1', id)).rows
+//         if (step.length) {
+//             await uniqRow('delete from steps where step_user = $1 and step_status = 1', id)
+//         } else {
+//             await uniqRow(`insert into steps (step_status, step_step, step_user) values (1, 1, $1)`, id)
+//             bot.on('message', async msg => {
+
+//             })
+//         }
+//     } catch (error) {
+//         console.log(error.message, 'Elon Taxi 🚖');
+//     }
+// })
+
+bot.onText(/\Elon Taxi 🚖/, async msg => {
+    const id = msg.chat.id
     try {
-        const id = msg.chat.id
-        await bot.sendMessage(id, 'boradigan manzilingizni kiriting', {
+        await bot.sendMessage(id, 'Boradigan manzilingizni kiriting', {
             reply_markup: {
                 remove_keyboard: true
             }
         })
-        const step = (await uniqRow('select * from step where step_user_telegram_id = $1 and step_status = 1 and step_step = 1', id)).rows
-        if (step.length) {
-            await uniqRow('delete from step where step_user_telegram_id = $1 and step_status = 1', id)
-        } else {
-            const createStep = await uniqRow(`insert into step (step_status, step_step, step_user_telegram_id) values (1, 1, $1)`, id)
-            bot.on('message', async msg => {
+        bot.on('message', async msg => {
+            const id2 = msg.chat.id
+            if (id === id2) {
                 
-            })
-        }
+                const uniqueID = uuidv4().split('-')[0]
+                
+                await uniqRow('insert into elontaxi (taxi_unique_id, taxi_now, taxi_owner) values ($1,$2,$3)', uniqueID, msg.text, id)
+                await bot.sendMessage(id, 'Siz turgan manzilni kiriting')
+                bot.on('message', async msg => {
+                    const id3 = msg.chat.id
+                    if (id2 === id3) {
+                        await uniqRow('update elontaxi set taxi_then = $1 where taxi_owner = $2', msg.text, id3)
+                        bot.sendMessage(id3, 'Odam sonini kiriting')
+                    }
+                })
+                bot.removeListener('message')
+            }
+        })
     } catch (error) {
-        console.log(error.message, 'Elon Taxi 🚖');
+        
     }
 })
 
-bot.on('message', async (msg) => {
-    const id = msg.chat.id
+// bot.on('message', async (msg) => {
+//     const id = msg.chat.id
+
+//     if (msg.text === 'Elon Taxi 🚖') {
+//         await bot.sendMessage(id, 'boradigan manzilingizni kiriting', {
+//             reply_markup: {
+//                 remove_keyboard: true
+//             }
+//         })
+        
+//         bot.on('message', async msg => {
+//             await uniqRow('insert into elontaxi (taxi_now, taxi_owner) values ($1,$2)', msg.text, id)
+//         })
+
+//     }
     
-})
+// })
+
+// bot.on('message', async msg => {
+//     const id = msg.chat.id
+
+//     await uniqRow('insert into elontaxi (taxi_now) values ($1)', id)
+
+//     const taxi = await uniqRow('select * from elontaxi where taxi_owner = $1', id)
+
+//     if (taxi.rows[0].taxi_status === 1) {
+//         await uniqRow('insert into elontaxi (taxi_now, taxi_owner) values ($1,$2)', msg.text, id)
+//         await bot.sendMessage(id, )
+//     }
+
+// })
+
+
+// https://receivesmsfast.com/messages?n=447360538792
